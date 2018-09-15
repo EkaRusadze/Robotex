ba450f9d47892401b078adb06d5f62189b526ae4
Eka <ekarusadze8@gmail.com>
2018-09-15 17:42:50 +0300

    Communication with the mainboard

---
+++ hardware/scripts/serial.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+from std_msgs.msg import String
+import serial
+from rattakiirus import wheelCalc, mainboardSpeedCalc
+
+
+ser = serial.Serial(
+    port='dev/ttyACM0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.EIGHTBITS
+)
+
+
+def movement(speed, angle, angularVelocity):
+    wheelSpeed0 = str(mainboardSpeedCalc(wheelCalc(0, speed, angle, angularVelocity)))
+    wheelSpeed1 = str(mainboardSpeedCalc(wheelCalc(1, speed, angle, angularVelocity)))
+    wheelSpeed2 = str(mainboardSpeedCalc(wheelCalc(2, speed, angle, angularVelocity)))
+
+    ser.write("sd:{0}:{1}:{2}:0\n".format(wheelSpeed0, wheelSpeed1, wheelSpeed2).encode())
+
+
+
+if __name__ == '__main__':
+    try:
+        movement(1, 0, 0)
+    except rospy.ROSInterruptException:
+        pass
